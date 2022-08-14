8ec0cb26655b744ddb4dedaa6125c6b5568101ee
NT <usedforfunny@gmail.com>
2022-08-14 19:29:33 +0200

    Initial Commit

---
+++ SwipeActionList/SwipeActionListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeActionListApp.swift
+//  SwipeActionList
+//
+//  Created by Alexandre Talatinian on 14/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwipeActionListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

fbb87efb917789a004beee1523af13593c3173aa
NT <usedforfunny@gmail.com>
2022-08-14 19:54:49 +0200

    Final Commit

--- SwipeActionList/ContentView.swift
+++ SwipeActionList/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nameList = ["Cancer", "Capricorne", "Sagittaire", "Taureau", "Balance"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List {
+                ForEach(nameList, id: \.self) { name in
+                    Text(name)
+                        .padding()
+                    
+                    // SWIPE ACTION 1
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                // code to delete
+                            } label: {
+                                Label("delete", systemImage: "trash")
+                            }
+                        }
+                    
+                    // SWIPE ACTION 2
+                        .swipeActions {
+                            Button {
+                                // code to edit
+                            } label: {
+                                Label("edit", systemImage: "square.and.pencil")
+                            }
+                            .tint(.blue)
+                        }
+                    
+                    // SWIPE ACTION 3
+                        .swipeActions(edge: .leading) {
+                            Button {
+                                // code for mic
+                            } label: {
+                                Label("mic", systemImage: "mic")
+                            }
+                            .tint(.green)
+                        }
+                }
+            } // LIST
+        } // VSTACK
     }
 }
 
